f61a7f319033a17eb072b528cc0a969ceeaa7ebf
hsyou <hsyou@incruit.com>
2021-09-06 11:06:26 +0900

    - mapi 추가

---
+++ Sources/whySPM/MAPI.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by icopy on 2021/09/06.
+//
+
+import Foundation
+
+public func getName() -> String {
+    return "Alamo"
+}
--- Sources/whySPM/whySPM.swift
+++ Sources/whySPM/whySPM.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 struct whySPM {
     var text = "Hello, World!"
 }
